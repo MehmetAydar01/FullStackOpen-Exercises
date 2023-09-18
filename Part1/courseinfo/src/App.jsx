f1da7fb8cac866efd0461e83f99b0a16799d7c18
Mehmet Aydar <90221996+MehmetAydar01@users.noreply.github.com>
2023-09-18 16:22:06 +0000

    part1--exercises-1.3

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -34,18 +34,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header head={course} />
-      <Content exercise1={exercises1} parts1={part1} exercise2={exercises2} parts2={part2} exercise3={exercises3} parts3={part3} />
-      <Total sum={ exercises1 + exercises2 + exercises3 } />
+      <Content exercise1={part1.exercises} parts1={part1.name} exercise2={part2.exercises} parts2={part2.name} exercise3={part3.exercises} parts3={part3.name} />
+      <Total sum={ part1.exercises + part2.exercises + part3.exercises } />
     </div>
   )
 }
