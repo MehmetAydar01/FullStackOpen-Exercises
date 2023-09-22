046e5035b9a4818983d7e80cbb30c65b9f260d11
Mehmet Aydar <90221996+MehmetAydar01@users.noreply.github.com>
2023-09-22 21:37:41 +0000

    part1--exercises-1.6

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react"
 
+const Head1 = props => <h1>{props.text}</h1>
+const Head2 = props => <h2>{props.text}</h2>
 
-function App() {
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>    
+      {props.text} {props.value}
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const mySetGood = () => {
+    setGood(good + 1)
+  }
+
+  const mySetNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const mySetBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-
+      <Head1 text='give feedback' />
+      <Button handleClick={mySetGood} text='good' />
+      <Button handleClick={mySetNeutral} text='neutral' />
+      <Button handleClick={mySetBad} text='bad' />
+      <Head2 text='statistics' />
+      <Statistics text='good' value={good} />
+      <Statistics text='neutral' value={neutral} />
+      <Statistics text='bad' value={bad} />
     </div>
   )
 }
