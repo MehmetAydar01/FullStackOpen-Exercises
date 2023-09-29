964ec74fe94cff7ed31949d5d25a88f17af3586f
Mehmet Aydar <90221996+MehmetAydar01@users.noreply.github.com>
2023-09-29 00:53:09 +0000

    Part2--exercises-2.5-component export modules

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -1,33 +1,5 @@
 
-const Header = ({ course }) => <h1>{course.name}</h1>
-
-const Content = ({ partContent }) => <div>{partContent.name} {partContent.exercises}</div>
-
-const TotalExercises = ({ sum }) => {
-  const total = sum.parts.reduce((s, p) => {
-    console.log('s : ', s, 'p : ', p)
-    return s + p.exercises
-  }, 0)
-
-  return (
-    <div>
-      <strong>total of {total} exercises</strong>
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      {course.parts.map(part =>
-        <Content key={part.id} partContent={part} />
-      )}
-      <TotalExercises sum={course} />
-    </>
-  )
-}
-
+import Course from './course/Course'
 
 const App = () => {
   const courses = [
---
+++ Part2/courseinfo/src/course/Course.jsx
@@ -0,0 +1,31 @@
+
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Content = ({ partContent }) => <div>{partContent.name} {partContent.exercises}</div>
+
+const TotalExercises = ({ sum }) => {
+  const total = sum.parts.reduce((s, p) => {
+    console.log('s : ', s, 'p : ', p)
+    return s + p.exercises
+  }, 0)
+
+  return (
+    <div>
+      <strong>total of {total} exercises</strong>
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+    return (
+        <>
+            <Header course={course} />
+            {course.parts.map(part =>
+                <Content key={part.id} partContent={part} />
+            )}
+            <TotalExercises sum={course} />
+        </>
+    )
+}
+
+export default Course
