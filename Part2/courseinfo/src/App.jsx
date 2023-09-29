ca0aed4112b1d440bd6ddafd1e7f03ece9b849cd
Mehmet Aydar <90221996+MehmetAydar01@users.noreply.github.com>
2023-09-29 00:35:27 +0000

    Part2--exercises-2.2-2.3

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -3,13 +3,27 @@ const Header = ({ course }) => <h1>{course.name}</h1>
 
 const Content = ({ partContent }) => <div>{partContent.name} {partContent.exercises}</div>
 
+const TotalExercises = ({ sum }) => {
+  const total = sum.parts.reduce((s, p) => {
+    console.log('s : ', s, 'p : ', p)
+    return s + p.exercises
+  }, 0)
+
+  return (
+    <div>
+      <strong>total of {total} exercises</strong>
+    </div>
+  )
+}
+
 const Course = ({ course }) => {
   return (
     <>
       <Header course={course} />
-      {course.parts.map(part => 
+      {course.parts.map(part =>
         <Content key={part.id} partContent={part} />
       )}
+      <TotalExercises sum={course} />
     </>
   )
 }
