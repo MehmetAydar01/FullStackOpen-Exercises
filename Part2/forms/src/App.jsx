ce1b59f97cdd66baa231cf414146a4470830fb82
Mehmet Aydar <90221996+MehmetAydar01@users.noreply.github.com>
2023-10-01 03:28:39 +0000

    Part2--exercises-2.6-The Phonebook Step1

--- Part2/forms/src/App.jsx
+++ Part2/forms/src/App.jsx
@@ -1,26 +1,50 @@
 import { useState } from "react"
 
 
-function App() {
-  const [inputValue, setInputValue] = useState('')
+const App = () => {
+  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas' }
+  ])
 
   const handleInputChange = (e) => {
-    console.log(e);
-    console.log(e.target)
+    // console.log(e.target.value)
+    setNewName(e.target.value)
+    // console.log(persons)
   }
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const personObject = {
+      id: persons.length + 1,
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
 
   return (
-    <>
+    <div>
       <h1>Part-2.6</h1>
-      <form>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div> name:
         <input
           type="text"
+          value={newName}
           onChange={handleInputChange}
         />
-        <button type="submit">Add</button>
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
       </form>
-    </>
+      <h2>Names</h2>
+      <ul>
+        {persons.map((person) => <li key={person.id}>{person.name}</li>)}
+      </ul>
+    </div>
   )
 }
 
